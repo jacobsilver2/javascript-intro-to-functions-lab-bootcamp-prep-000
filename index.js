e9aab330c1f1bf0f33aac7d58c409e11b0668e35
Jacob Silver <jacobsilver2@mac.com>
2017-11-30 14:21:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"} 
       else if (string.toUpperCase === string) {
-      return "YES INDEED!"} else {return "I love you, too."}
+      return "YES INDEED!"} else if {string === ""}
 }
